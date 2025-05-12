0b694f6c46cd3fba02aaf24721b7f3d192fa0aaa
Hugo <hhakem@broadinstitute.org>
2025-05-12 19:32:41 +0000

    feat: update download

--- fine_tune/scripts/utils_gears.py
+++ fine_tune/scripts/utils_gears.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import sys
 from pathlib import Path
 from zipfile import ZipFile
@@ -41,7 +42,8 @@ def dataverse_download(url: str, save_path: Path):
 
 def zip_data_download_wrapper(url: str, save_path: Path, data_path: Path):
     """
-    Wrapper for zip file download
+    Wrapper for zip file download.
+    Guarantee the zip extracted file is flatten and name after save_path.stem
 
     Args:
         url (str): the url of the dataset
@@ -54,91 +56,42 @@ def zip_data_download_wrapper(url: str, save_path: Path, data_path: Path):
     else:
         dataverse_download(url, save_path.with_suffix('.zip'))
         print_sys('Extracting zip file...')
+
+        # Make tmp dir to extract zip
+        tmp_dir = data_path / f"__temp_extract_{save_path.stem}"
+        tmp_dir.mkdir(parents=True, exist_ok=True)
+
         with ZipFile(save_path.with_suffix('.zip'), 'r') as zip:
-            zip.extractall(path = data_path)
+            zip.extractall(path = tmp_dir)
+
+        # Flatten path
+        flattened_path = flatten_single_child_dirs(tmp_dir)
+
+        # Final output directory
+        output_dir = data_path / save_path.stem
+        output_dir.mkdir(parents=True, exist_ok=True)
+
+        # Move contents of the final subfolder to output_dir
+        for item in flattened_path.iterdir():
+            shutil.move(item, output_dir)
+
+        # Clean up temp_dir
+        shutil.rmtree(tmp_dir)
+
         print_sys("Done!")
 
-
-# def load(self, data_name = None, data_path = None):
-#     """
-#     Load existing dataloader
-#     Use data_name for loading 'norman', 'adamson', 'dixit' datasets
-#     For other datasets use data_path
-
-#     Parameters
-#     ----------
-#     data_name: str
-#         Name of dataset
-#     data_path: str
-#         Path to dataset
-
-#     Returns
-#     -------
-#     None
-
-#     """
-
-#     if data_name in ['norman', 'adamson', 'dixit',
-#                         'replogle_k562_essential',
-#                         'replogle_rpe1_essential']:
-#         ## load from harvard dataverse
-#         if data_name == 'norman':
-#             url = 'https://dataverse.harvard.edu/api/access/datafile/6154020'
-#         elif data_name == 'adamson':
-#             url = 'https://dataverse.harvard.edu/api/access/datafile/6154417'
-#         elif data_name == 'dixit':
-#             url = 'https://dataverse.harvard.edu/api/access/datafile/6154416'
-#         elif data_name == 'replogle_k562_essential':
-#             ## Note: This is not the complete dataset and has been filtered
-#             url = 'https://dataverse.harvard.edu/api/access/datafile/7458695'
-#         elif data_name == 'replogle_rpe1_essential':
-#             ## Note: This is not the complete dataset and has been filtered
-#             url = 'https://dataverse.harvard.edu/api/access/datafile/7458694'
-#         data_path = os.path.join(self.data_path, data_name)
-#         zip_data_download_wrapper(url, data_path, self.data_path)
-#         self.dataset_name = data_path.split('/')[-1]
-#         self.dataset_path = data_path
-#         adata_path = os.path.join(data_path, 'perturb_processed.h5ad')
-#         self.adata = sc.read_h5ad(adata_path)
-
-#     elif os.path.exists(data_path):
-#         adata_path = os.path.join(data_path, 'perturb_processed.h5ad')
-#         self.adata = sc.read_h5ad(adata_path)
-#         self.dataset_name = data_path.split('/')[-1]
-#         self.dataset_path = data_path
-#     else:
-#         raise ValueError("data attribute is either norman, adamson, dixit "
-#                             "replogle_k562 or replogle_rpe1 "
-#                             "or a path to an h5ad file")
-
-#     self.set_pert_genes()
-#     print_sys('These perturbations are not in the GO graph and their '
-#                 'perturbation can thus not be predicted')
-#     not_in_go_pert = np.array(self.adata.obs[
-#                                 self.adata.obs.condition.apply(
-#                                 lambda x:not filter_pert_in_go(x,
-#                                     self.pert_names))].condition.unique())
-#     print_sys(not_in_go_pert)
-
-#     filter_go = self.adata.obs[self.adata.obs.condition.apply(
-#                             lambda x: filter_pert_in_go(x, self.pert_names))]
-#     self.adata = self.adata[filter_go.index.values, :]
-#     pyg_path = os.path.join(data_path, 'data_pyg')
-#     if not os.path.exists(pyg_path):
-#         os.mkdir(pyg_path)
-#     dataset_fname = os.path.join(pyg_path, 'cell_graphs.pkl')
-
-#     if os.path.isfile(dataset_fname):
-#         print_sys("Local copy of pyg dataset is detected. Loading...")
-#         self.dataset_processed = pickle.load(open(dataset_fname, "rb"))
-#         print_sys("Done!")
-#     else:
-#         self.ctrl_adata = self.adata[self.adata.obs['condition'] == 'ctrl']
-#         self.gene_names = self.adata.var.gene_name
-
-
-#         print_sys("Creating pyg object for each cell in the data...")
-#         self.create_dataset_file()
-#         print_sys("Saving new dataset pyg object at " + dataset_fname)
-#         pickle.dump(self.dataset_processed, open(dataset_fname, "wb"))
-#         print_sys("Done!")
+def flatten_single_child_dirs(start_path: Path) -> Path:
+    """
+    Given a directory, recursively descend while there's only one subdirectory and no files.
+    Returns the deepest such directory.
+    """
+    current = start_path
+    while True:
+        children = list(current.iterdir())
+        dirs = [d for d in children if d.is_dir()]
+        files = [f for f in children if f.is_file()]
+        if len(dirs) == 1 and not files:
+            current = dirs[0]
+        else:
+            break
+    return current
